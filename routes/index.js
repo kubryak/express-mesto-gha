c54bb2e240931586ff8b1a4229d2e876f1adac74
kubryak <al.lcn@yandex.ru>
2023-06-22 21:19:21 +0500

    fix: Исправить ошибки автотестов

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,10 @@ const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const { login, createUser } = require('../controllers/users');
 const NotFoundError = require('../errors/NotFoundError');
+const celebrates= require('../middlewares/celebrate');
 
-router.use('/signin', login);
-router.use('/signup', createUser);
+router.use('/signin', celebrates.validateCreateAndLoginUser, login);
+router.use('/signup', celebrates.validateCreateAndLoginUser, createUser);
 router.use(auth);
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
